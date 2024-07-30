86075c1dea46def26f43df2cee499e73144461e5
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-07-30 12:33:57 +0545

    Initial

--- Auth/views.py
+++ Auth/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate,logout
+from django.contrib.auth import authenticate,logout,login
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def log_in(request):
-    # checck user is authenticated or not 
+    # check user is authenticated or not 
     if request.user.is_authenticated:
         return redirect('/home')
     if request.method=='POST':
@@ -18,9 +19,6 @@ def log_in(request):
                 return render(request, 'Auth/login.html', {'error': '* Invalid Credentials'})
         else:
             return render(request, 'Auth/login.html', {'error': '* Please fill all the fields'})
-        print(username,password)
-
-        # return redirect('/home')
     return render(request, 'Auth/login.html')
 
 
@@ -34,14 +32,14 @@ def sign_up(request):
         email=request.POST.get('email').strip()
         password=request.POST.get('password')
         if firstname and lastname and username and email and password:
-            # create the user 
-            
-
+            # create the user
+            print(firstname,lastname,username,email,password)
+            user=User.objects.create_user(username=username,email=email,password=password,first_name=firstname,last_name=lastname)
+            user.save()
+            login(request,user)
+            return redirect('/home')
         else:
             return render(request, 'Auth/signup.html', {'error': '* Please fill all the fields'})
-        print(firstname,lastname,username,email,password)
-        
-    
     return render(request, 'Auth/signup.html')
 
 def log_out(request):
