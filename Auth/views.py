eef0a15f6f9f42c6262aa132d2936f6206d5f627
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-07-30 11:28:51 +0545

    Initial

--- Auth/views.py
+++ Auth/views.py
@@ -5,8 +5,28 @@ def log_in(request):
     # checck user is authenticated or not 
     if request.user.is_authenticated:
         return redirect('/home')
-    return render(request, 'login.html')
+    if request.method=='POST':
+        username=request.POST.get('username').strip()
+        password=request.POST.get('password')
+        if username and password:
+            return render(request, 'Auth/login.html', {'error': '* Please fill all the fields'})
+        print(username,password)
+        # return redirect('/home')
+    return render(request, 'Auth/login.html')
 
 
 def sign_up(request):
-    return render(request, 'signup.html')
+    if request.user.is_authenticated:
+        return redirect('/home')
+    if request.method=='POST':
+        firstname=request.POST.get('firstname').strip()
+        lastname=request.POST.get('lastname').strip()
+        username=request.POST.get('username').strip()
+        email=request.POST.get('email').strip()
+        password=request.POST.get('password')
+        if firstname and lastname and username and email and password:
+            return render(request, 'Auth/signup.html', {'error': '* Please fill all the fields'})
+        print(firstname,lastname,username,email,password)
+        
+    
+    return render(request, 'Auth/signup.html')
